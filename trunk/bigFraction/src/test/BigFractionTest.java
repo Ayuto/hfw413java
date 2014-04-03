16e35a2b4f1f7f037c110bdf5f08f87f914968df
ole.vonbargen@googlemail.com <ole.vonbargen@googlemail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-04-03 21:47:51 +0000

    - Parse hinzugefügt (für Format [Nenner]/[Zähler]) - Testfälle dafür hinzugefügt

--- trunk/bigFraction/src/test/BigFractionTest.java
+++ trunk/bigFraction/src/test/BigFractionTest.java
@@ -1,9 +1,8 @@
 package test;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import model.BigFraction;
@@ -14,6 +13,7 @@ public class BigFractionTest {
 
 	@Test
 	public void test() {
+		final BigFraction half = BigFraction.create(BigInteger.ONE, BigInteger.valueOf(2));
 		final BigFraction quarter1 = BigFraction.create(BigInteger.valueOf(1), BigInteger.valueOf(4));
 		final BigFraction quarter2 = BigFraction.create(BigInteger.valueOf(-2), BigInteger.valueOf(-8));
 		assertEquals(quarter1, quarter2);
@@ -22,5 +22,10 @@ public class BigFractionTest {
 		//assertEquals(BigFraction.parse("1/16"), quarter1.multiply(quarter2));
 		assertEquals("1/4", quarter1.toString());
 		assertEquals("1/16", quarterSquare.toString());
+		
+		assertEquals(quarter1, BigFraction.parse("1/4"));
+		assertEquals(quarterSquare, BigFraction.parse("2/32"));
+		assertEquals(half, BigFraction.parse("2/4"));
+		assertEquals(quarter2, BigFraction.parse(quarter2.toString()));
 	}
 }
