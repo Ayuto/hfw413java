97533babd142e603a40fb485f9a02a91de4b4765
ole.vonbargen@googlemail.com <ole.vonbargen@googlemail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-04-06 09:38:26 +0000

    BigFraction.parse() hinzugefügt (Format [dezimalzahl]E[negativer Exponent] Testfälle hinzugefügt

--- bigFraction/src/test/BigFractionTest.java
+++ bigFraction/src/test/BigFractionTest.java
@@ -1,6 +1,5 @@
 package test;
 
-
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigInteger;
@@ -14,29 +13,49 @@ public class BigFractionTest {
 
 	@Test
 	public void test() {
-		final BigFraction half = BigFraction.create(BigInteger.ONE, BigInteger.valueOf(2));
-		final BigFraction quarter1 = BigFraction.create(BigInteger.valueOf(1), BigInteger.valueOf(4));
-		final BigFraction quarter2 = BigFraction.create(BigInteger.valueOf(-2), BigInteger.valueOf(-8));
+		final BigFraction half = BigFraction.create(BigInteger.ONE,
+				BigInteger.valueOf(2));
+		final BigFraction quarter1 = BigFraction.create(BigInteger.valueOf(1),
+				BigInteger.valueOf(4));
+		final BigFraction quarter2 = BigFraction.create(BigInteger.valueOf(-2),
+				BigInteger.valueOf(-8));
 		assertEquals(quarter1, quarter2);
-		
-		final BigFraction quarterSquare = BigFraction.create(BigInteger.ONE, BigInteger.valueOf(16));
+
+		final BigFraction quarterSquare = BigFraction.create(BigInteger.ONE,
+				BigInteger.valueOf(16));
 		assertEquals(BigFraction.parse("1/16"), quarter1.multiply(quarter2));
 		assertEquals("1/4", quarter1.toString());
 		assertEquals("1/16", quarterSquare.toString());
-		
-		try{
+
+		try {
 			BigFraction.parse("Hugo ist doof!");
 			Assert.fail();
-		} catch (NumberFormatException e){}
-		
+		} catch (NumberFormatException e) {
+		}
+
+		try {
+			BigFraction.parse("");
+			Assert.fail();
+		} catch (NumberFormatException e) {
+		}
+
+		try {
+			BigFraction.parse(null);
+			Assert.fail();
+		} catch (NullPointerException e) {
+		}
+
 		assertEquals(quarter1, BigFraction.parse("1/4"));
 		assertEquals(quarterSquare, BigFraction.parse("2/32"));
 		assertEquals(half, BigFraction.parse("2/4"));
 		assertEquals(quarter2, BigFraction.parse(quarter2.toString()));
-		
+
 		assertEquals(quarter1, BigFraction.parse("0.25"));
 		assertEquals("1/8", BigFraction.parse("0.125").toString());
 		assertEquals(half, BigFraction.parse("0000.5000"));
 		assertEquals(quarterSquare, BigFraction.parse(".0625"));
+
+		assertEquals(quarter1, BigFraction.parse("25E2"));
+		assertEquals("1/1000", BigFraction.parse("1E3").toString());
 	}
 }
