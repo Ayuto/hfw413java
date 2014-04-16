194622ec90d497dabf8f710697548bce01f54ff5
ole.vonbargen@googlemail.com <ole.vonbargen@googlemail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-04-16 11:17:30 +0000

    cleanup

--- trunk/partsList2014/src/Starter.java
+++ trunk/partsList2014/src/Starter.java
@@ -3,27 +3,25 @@ import java.awt.Toolkit;
 
 import javax.swing.UIManager;
 
-import view.View;
-
 import model.PartsList;
-
+import view.View;
 
 public class Starter {
 
 	public static void main(final String[] args) {
 		try {
-		  UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch(final Exception e) {
-		  e.printStackTrace();
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (final Exception e) {
+			e.printStackTrace();
 		}
 		final View view = View.create(PartsList.create());
 
-		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		final Dimension screenSize = Toolkit.getDefaultToolkit()
+				.getScreenSize();
 		final Dimension viewSize = view.getSize();
-		final double leftMargin = (screenSize.getWidth() - viewSize.getWidth())/2;
-		final double topMargin = (screenSize.getHeight() - viewSize.getHeight())/2;
-		view.setLocation((int)leftMargin,(int)topMargin);
+		final double leftMargin = (screenSize.getWidth() - viewSize.getWidth()) / 2;
+		final double topMargin = (screenSize.getHeight() - viewSize.getHeight()) / 2;
+		view.setLocation((int) leftMargin, (int) topMargin);
 		view.setVisible(true);
 	}
 
