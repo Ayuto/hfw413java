d23d9349acb7dced89f16fe874c03d7393cd490f
ole.vonbargen@googlemail.com <ole.vonbargen@googlemail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-04-16 11:10:53 +0000

    Testfall hinzugefügt

--- trunk/bigFraction/src/model/BigFraction.java
+++ trunk/bigFraction/src/model/BigFraction.java
@@ -45,13 +45,12 @@ public class BigFraction {
 			throw new NullPointerException();
 
 		// Format <enumerator>/<denominator> and Format /<denominator>
-		if (str.contains(FRACTION_SEPERATOR)) {
-			final String[] parts = str.split(FRACTION_SEPERATOR);
+		if (str.contains(BigFraction.FRACTION_SEPERATOR)) {
+			final String[] parts = str.split(BigFraction.FRACTION_SEPERATOR);
 			// Format /<denominator>
-			if (str.indexOf(FRACTION_SEPERATOR) == 0) {
+			if (str.indexOf(BigFraction.FRACTION_SEPERATOR) == 0)
 				return BigFraction.create(BigInteger.ONE, new BigInteger(
 						parts[1]));
-			}
 
 			// Format <enumerator>/<denominator>
 			if (parts.length != 2)
@@ -62,39 +61,37 @@ public class BigFraction {
 		}
 
 		// Format decimal number e.g. 5.2135 or .125
-		if (str.contains(DECIMAL_SEPERATOR)) {
+		if (str.contains(BigFraction.DECIMAL_SEPERATOR)) {
 			final int decimalpositions = (str.length() - 1)
-					- str.indexOf(DECIMAL_SEPERATOR);
+					- str.indexOf(BigFraction.DECIMAL_SEPERATOR);
 
 			return BigFraction.create(
-					new BigInteger(str.replace(DECIMAL_SEPERATOR, "")),
-					BigInteger.valueOf((long) Math.pow(DECIMAL_BASE,
-							decimalpositions)));
+					new BigInteger(str.replace(BigFraction.DECIMAL_SEPERATOR,
+							"")), BigInteger.valueOf((long) Math.pow(
+							BigFraction.DECIMAL_BASE, decimalpositions)));
 		}
 
 		// Format <number>E<decimal exponent>
-		if (str.contains(DECIMAL_POW_SEPERATOR)) {
+		if (str.contains(BigFraction.DECIMAL_POW_SEPERATOR)) {
 			final String[] parts;
-			if (str.startsWith(DECIMAL_POW_SEPERATOR)) {
+			if (str.startsWith(BigFraction.DECIMAL_POW_SEPERATOR)) {
 				parts = new String[2];
 				parts[0] = "1";
-				parts[1] = str.replace(DECIMAL_POW_SEPERATOR, "");
+				parts[1] = str.replace(BigFraction.DECIMAL_POW_SEPERATOR, "");
 			} else {
-				parts = str.split(DECIMAL_POW_SEPERATOR);
+				parts = str.split(BigFraction.DECIMAL_POW_SEPERATOR);
 			}
 			if (parts.length != 2)
 				throw new NumberFormatException();
-			if (Long.valueOf(parts[1]) < 0) {
-				return BigFraction.create(
-						new BigInteger(parts[0]),
-						BigInteger.valueOf((long) Math.pow(DECIMAL_BASE,
+			if (Long.valueOf(parts[1]) < 0)
+				return BigFraction.create(new BigInteger(parts[0]), BigInteger
+						.valueOf((long) Math.pow(BigFraction.DECIMAL_BASE,
 								Long.valueOf(parts[1]) * -1)));
-			} else {
+			else
 				return BigFraction.create(new BigInteger(parts[0])
 						.multiply(BigInteger.valueOf((long) Math.pow(
-								DECIMAL_BASE, Long.valueOf(parts[1])))),
-						BigInteger.ONE);
-			}
+								BigFraction.DECIMAL_BASE,
+								Long.valueOf(parts[1])))), BigInteger.ONE);
 		}
 
 		// Format 1
@@ -141,7 +138,8 @@ public class BigFraction {
 	 * Returns true if the given object <argument> represents the same fraction
 	 * like <this>.
 	 */
-	public boolean equals(Object argument) {
+	@Override
+	public boolean equals(final Object argument) {
 		if (argument instanceof BigFraction) {
 			final BigFraction argumentAsBigFraction = (BigFraction) argument;
 			return this
@@ -156,7 +154,7 @@ public class BigFraction {
 	/**
 	 * Adds <value> to <this> and returns the result as a new object.
 	 */
-	public BigFraction add(BigFraction value) {
+	public BigFraction add(final BigFraction value) {
 		return BigFraction.create(
 				this.getEnumerator()
 						.multiply(value.getDenominator())
@@ -168,7 +166,7 @@ public class BigFraction {
 	/**
 	 * Subtracts <value> from <this> and returns the result as a new object.
 	 */
-	public BigFraction subtract(BigFraction value) {
+	public BigFraction subtract(final BigFraction value) {
 		return BigFraction.create(
 				this.getEnumerator()
 						.multiply(value.getDenominator())
@@ -181,7 +179,7 @@ public class BigFraction {
 	/**
 	 * Multiplies <this> and <factor> and returns the result as a new object.
 	 */
-	public BigFraction multiply(BigFraction factor) {
+	public BigFraction multiply(final BigFraction factor) {
 		return BigFraction.create(
 				this.getEnumerator().multiply(factor.getEnumerator()), this
 						.getDenominator().multiply(factor.getDenominator()));
@@ -191,9 +189,10 @@ public class BigFraction {
 	 * Divides <this> by <divisor> and returns the result as a new object.
 	 * Throws an IllegalArgumentException if the <divisor> equals zero.
 	 */
-	public BigFraction divide(BigFraction divisor) {
-		if (divisor.equals(ZERO))
-			throw new IllegalArgumentException(DIVISION_BY_ZERO_ERROR);
+	public BigFraction divide(final BigFraction divisor) {
+		if (divisor.equals(BigFraction.ZERO))
+			throw new IllegalArgumentException(
+					BigFraction.DIVISION_BY_ZERO_ERROR);
 		return BigFraction.create(
 				this.getEnumerator().multiply(divisor.getDenominator()), this
 						.getDenominator().multiply(divisor.getEnumerator()));
@@ -202,8 +201,9 @@ public class BigFraction {
 	/**
 	 * Stringifies the fraction <this>.
 	 */
+	@Override
 	public String toString() {
-		return this.getEnumerator().toString() + FRACTION_SEPERATOR
+		return this.getEnumerator().toString() + BigFraction.FRACTION_SEPERATOR
 				+ this.getDenominator().toString();
 	}
 }
