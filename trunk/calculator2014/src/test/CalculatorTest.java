dab4ccdd74d548f399f0ce67f8d5a971a21b7b33
ayuto@web.de <ayuto@web.de@c11406e9-745a-c773-d383-491c0784de40>
2014-05-03 15:55:22 +0000

    Testfälle für Subtraction und Division

--- trunk/calculator2014/src/test/CalculatorTest.java
+++ trunk/calculator2014/src/test/CalculatorTest.java
@@ -5,7 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import model.Addition;
+<<<<<<< .mine
+import model.DivisionByZeroException;
+import model.Subtraction;
+import model.Division;
 import model.Multiplication;
+=======
+import model.Multiplication;
+>>>>>>> .r51
 import model.Variable;
 
 public class CalculatorTest {
@@ -81,7 +88,20 @@ public class CalculatorTest {
 		Assert.assertEquals(1, ad.getValue());
 		// (6+6)+6
 		Assert.assertEquals(18, aaa.getValue());
+	}
+	
+	@Test
+	public void testSubtract()
+	{
+		Subtraction aa = Subtraction.create(a, a);
+		// 5-5
+		Assert.assertEquals(0, aa.getValue());
 		
+<<<<<<< .mine
+		Subtraction ab = Subtraction.create(a, b);
+		// 5-3
+		Assert.assertEquals(2, ab.getValue());
+=======
 	}
 	
 	@Test
@@ -89,7 +109,37 @@ public class CalculatorTest {
 		Multiplication aa = Multiplication.create(a, a);
 		// 5*5
 		Assert.assertEquals(25, aa.getValue());
+>>>>>>> .r51
 		
+<<<<<<< .mine
+		Subtraction ac = Subtraction.create(a, c);
+		// 5-(-2)
+		Assert.assertEquals(7, ac.getValue());
+		
+		Subtraction ad = Subtraction.create(a, d);
+		// 5-(-5)
+		Assert.assertEquals(10, ad.getValue());
+		
+		Subtraction cd = Subtraction.create(c, d);
+		// (-2)-(-5)
+		Assert.assertEquals(3, cd.getValue());
+		
+		Subtraction aaa = Subtraction.create(aa, a);
+		// (5-5)-5
+		Assert.assertEquals(-5, aaa.getValue());
+		
+		a.up();
+		// 6-6
+		Assert.assertEquals(0, aa.getValue());
+		// 6-3
+		Assert.assertEquals(3, ab.getValue());
+		// 6-(-2)
+		Assert.assertEquals(8, ac.getValue());
+		// 6-(-5)
+		Assert.assertEquals(11, ad.getValue());
+		// (6-6)-6
+		Assert.assertEquals(-6, aaa.getValue());
+=======
 		Multiplication ab = Multiplication.create(a, b);
 		// 5*3
 		Assert.assertEquals(15, ab.getValue());
@@ -124,5 +174,93 @@ public class CalculatorTest {
 		Assert.assertEquals(-72, aac.getValue());
 		
 		
+>>>>>>> .r51
+	}
+	
+	@Test
+    public void testMultiplication() {
+            Multiplication aa = Multiplication.create(a, a);
+            // 5*5
+            Assert.assertEquals(25, aa.getValue());
+           
+            Multiplication ab = Multiplication.create(a, b);
+            // 5*3
+            Assert.assertEquals(15, ab.getValue());
+           
+            Multiplication ac = Multiplication.create(a, c);
+            // 5*(-2)
+            Assert.assertEquals(-10, ac.getValue());
+           
+            Multiplication ad = Multiplication.create(a, d);
+            // 5*(-5)
+            Assert.assertEquals(-25, ad.getValue());
+           
+            Multiplication cd = Multiplication.create(c, d);
+            // (-2)*(-5)
+            Assert.assertEquals(10, cd.getValue());
+           
+            Multiplication aac = Multiplication.create(aa, c);
+            // (5*5)*(-2)
+            Assert.assertEquals(-50, aac.getValue());
+           
+           
+            a.up();
+            // 6*6
+            Assert.assertEquals(36, aa.getValue());
+            // 6*3
+            Assert.assertEquals(18, ab.getValue());
+            // 6*(-2)
+            Assert.assertEquals(-12, ac.getValue());
+            // 6*(-5)
+            Assert.assertEquals(-30, ad.getValue());
+            // (6*6)*(-2)
+            Assert.assertEquals(-72, aac.getValue());
+	}
+	
+	@Test
+	public void testDivision()
+	{
+		Division aa = Division.create(a, a);
+		// 5/5
+		Assert.assertEquals(1, aa.getValue());
+		
+		Division ab = Division.create(a, b);
+		// 5/3
+		Assert.assertEquals(1, ab.getValue());
+		
+		Division ac = Division.create(a, c);
+		// 5/(-2)
+		Assert.assertEquals(-2, ac.getValue());
+		
+		Division ad = Division.create(a, d);
+		// 5/(-5)
+		Assert.assertEquals(-1, ad.getValue());
+		
+		Division cd = Division.create(c, d);
+		// (-2)/(-5)
+		Assert.assertEquals(0, cd.getValue());
+		
+		Division aaa = Division.create(aa, a);
+		// (5/5)/5
+		Assert.assertEquals(0, aaa.getValue());
+		
+		a.up();
+		// 6/6
+		Assert.assertEquals(1, aa.getValue());
+		// 6/3
+		Assert.assertEquals(2, ab.getValue());
+		// 6/(-2)
+		Assert.assertEquals(-3, ac.getValue());
+		// 6/(-5)
+		Assert.assertEquals(-1, ad.getValue());
+		// (6/6)/6
+		Assert.assertEquals(0, aaa.getValue());
+
+		// 6/0
+		try {
+			Division.create(a, Variable.createVariable("NULL"));
+			Assert.fail();
+		} catch (final DivisionByZeroException e) {
+		}
 	}
 }
