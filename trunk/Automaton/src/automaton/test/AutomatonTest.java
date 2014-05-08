40ddce00e3cfb30c8abede2e21ded5393a7f302e
ole.vonbargen@googlemail.com <ole.vonbargen@googlemail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-05-08 11:46:23 +0000

    - Testfall hinzugefügt

--- trunk/Automaton/src/automaton/test/AutomatonTest.java
+++ trunk/Automaton/src/automaton/test/AutomatonTest.java
@@ -12,30 +12,36 @@ public class AutomatonTest {
 	private Automaton m2;
 	private Automaton m3;
 	private Automaton m4;
+	private Automaton m5;
 	
 	@Before
 	public void setUp() throws Exception {
-		m1 = Automaton.create();
-		m2 = Automaton.create('a');
-		m3 = Automaton.create('a');
-		m3.getStart().add('a', m3.getStart());
-		m4 = Automaton.create('b');
-		m4.getStart().add('a', m4.getStart());
+		this.m1 = Automaton.create();
+		this.m2 = Automaton.create('a');
+		this.m3 = Automaton.create('a');
+		this.m3.getStart().add('a', this.m3.getStart());
+		this.m4 = Automaton.create('b');
+		this.m4.getStart().add('a', this.m4.getStart());
+		this.m5 = Automaton.create('a');
+		this.m5.getStart().add('b', this.m5.getEnd());
+		this.m5.setOptional(true);
+		this.m5.iterated();
 	}
 
 	@Test
 	public void testRecognizes() {
-		Assert.assertFalse(m1.recognizes("irgendwas"));
-		Assert.assertFalse(m2.recognizes("aa"));
-		Assert.assertTrue(m2.recognizes("a"));
+		Assert.assertFalse(this.m1.recognizes("irgendwas"));
+		Assert.assertFalse(this.m2.recognizes("aa"));
+		Assert.assertTrue(this.m2.recognizes("a"));
 		String input = "a";
 		for (int i = 0; i < 30; i++) {
-			Assert.assertTrue(m3.recognizes(input));
+			Assert.assertTrue(this.m3.recognizes(input));
 			input = input + "a";
 		}
-		Assert.assertFalse(m4.recognizes(""));
-		Assert.assertTrue(m4.recognizes("aaaaaab"));
-		Assert.assertFalse(m4.recognizes("aaba"));
+		Assert.assertFalse(this.m4.recognizes(""));
+		Assert.assertTrue(this.m4.recognizes("aaaaaab"));
+		Assert.assertFalse(this.m4.recognizes("aaba"));
+		Assert.assertTrue(this.m5.recognizes("abbbaaaaabaabba"));
 	}
 
 }
