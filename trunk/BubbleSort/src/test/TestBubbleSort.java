a3975812eb8e3e406fd1065af52629881cd459a8
ole.vonbargen@gmail.com <ole.vonbargen@gmail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-10-24 13:43:35 +0000

    - BubbleSort is working now - Added generic StopCommand - JavaDocs and JUnit testcases are missing (TODO)

--- trunk/BubbleSort/src/test/TestBubbleSort.java
+++ trunk/BubbleSort/src/test/TestBubbleSort.java
@@ -4,26 +4,50 @@ import java.util.LinkedList;
 import java.util.List;
 
 import model.BubbleSortManager;
+import model.BufferEntry;
 import model.IntegerWrapper;
-import model.IntegerWrapperStopCommand;
+import model.StopCommand;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestBubbleSort {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Test
-	public void test() {
-		final List<IntegerWrapper> l1 = new LinkedList<IntegerWrapper>();
+	public void testNoSwitch() {
+		final List<BufferEntry<IntegerWrapper>> l1 = new LinkedList<BufferEntry<IntegerWrapper>>();
 		l1.add(new IntegerWrapper(1));
 		l1.add(new IntegerWrapper(2));
 		l1.add(new IntegerWrapper(3));
 		l1.add(new IntegerWrapper(4));
-		l1.add(IntegerWrapperStopCommand.getInstance());
+		l1.add(new StopCommand<IntegerWrapper>());
 		
-		final BubbleSortManager<IntegerWrapper> manager = new BubbleSortManager<IntegerWrapper>(l1);
-		final List<IntegerWrapper> result = manager.sort();
+		final BubbleSortManager manager = new BubbleSortManager(l1);
+		final List<BufferEntry<IntegerWrapper>> result = manager.sort();
+		l1.remove(l1.size() - 1);
 		Assert.assertEquals(l1, result);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void testAllSwitch() {
+		final List<BufferEntry<IntegerWrapper>> l1 = new LinkedList<BufferEntry<IntegerWrapper>>();
+		l1.add(new IntegerWrapper(4));
+		l1.add(new IntegerWrapper(3));
+		l1.add(new IntegerWrapper(2));
+		l1.add(new IntegerWrapper(1));
+		l1.add(new StopCommand<IntegerWrapper>());
+		
+		final List<BufferEntry<IntegerWrapper>> lresult = new LinkedList<BufferEntry<IntegerWrapper>>();
+		lresult.add(new IntegerWrapper(1));
+		lresult.add(new IntegerWrapper(2));
+		lresult.add(new IntegerWrapper(3));
+		lresult.add(new IntegerWrapper(4));
+		
+		final BubbleSortManager manager = new BubbleSortManager(l1);
+		
+		Assert.assertEquals(lresult, manager.sort());
 	}
 
 }
