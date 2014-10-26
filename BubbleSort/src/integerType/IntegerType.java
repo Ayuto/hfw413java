c3843cb6b7f1573bb4b7ba458f09b579ddbd2c67
ayuto@web.de <ayuto@web.de>
2014-10-26 12:48:38 +0000

    - Added doc strings for BubbleSort

--- BubbleSort/src/integerType/IntegerType.java
+++ BubbleSort/src/integerType/IntegerType.java
@@ -2,6 +2,9 @@ package integerType;
 
 import model.BufferEntry;
 
+/**
+ * A single integer buffer entry.
+ */
 public interface IntegerType extends BufferEntry<IntegerType> {
 
 }
--- BubbleSort/src/model/BubbleSortManager.java
+++ BubbleSort/src/model/BubbleSortManager.java
@@ -8,6 +8,9 @@ import lock.Lock;
 import buffer.AbstractBuffer;
 import buffer.BufferSolution;
 
+/**
+ * A class to sort a list by using the bubble sort algorithm.
+ */
 public class BubbleSortManager<T extends BufferEntry<T>> {
 
 	private final List<T> input;
@@ -16,6 +19,10 @@ public class BubbleSortManager<T extends BufferEntry<T>> {
 	private AbstractLock mutex;
 	private AbstractLock lockFinished;
 
+	/**
+	 * Sorts a list from the smallest to the highest value.
+	 */
+	// TODO: Accept every collection?
 	public BubbleSortManager(final List<T> input) {
 		this.input = input;
 		this.result = new BufferSolution<T>();
@@ -24,6 +31,11 @@ public class BubbleSortManager<T extends BufferEntry<T>> {
 		this.lockFinished = new Lock(true);
 	}
 
+	/**
+	 * Starts the sorting process.
+	 * 
+	 * @return The sorted list.
+	 */
 	public List<T> sort() {
 		for (final T element : this.input) {
 			this.result.put(element);
@@ -39,6 +51,9 @@ public class BubbleSortManager<T extends BufferEntry<T>> {
 		return result;
 	}
 
+	/**
+	 * Starts a new BubbleSort thread.
+	 */
 	void createNewThread() {
 		this.mutex.lock();
 		final AbstractBuffer<T> input = this.result;
@@ -48,6 +63,10 @@ public class BubbleSortManager<T extends BufferEntry<T>> {
 		this.mutex.unlock();
 	}
 
+	/**
+	 * This operation should be called by a terminating BubbleSort thread to
+	 * finalize it.
+	 */
 	void threadEnds() {
 		this.mutex.lock();
 		this.runningThreads--;
