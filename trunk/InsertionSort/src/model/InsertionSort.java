e97b31e54f692f8acf835a808e28d1be8c976fd5
ayuto@web.de <ayuto@web.de@c11406e9-745a-c773-d383-491c0784de40>
2014-11-02 14:22:02 +0000

    - Added doc strings for InsertionSort

--- trunk/InsertionSort/src/model/InsertionSort.java
+++ trunk/InsertionSort/src/model/InsertionSort.java
@@ -3,6 +3,12 @@ package model;
 import buffer.AbstractBuffer;
 import buffer.BufferSolution;
 
+/**
+ * A Runnable which does one run of an insertion sort.
+ * 
+ * @param <T>
+ *            Type of the elements of the input.
+ */
 public class InsertionSort<T extends BufferEntry<T>> implements Runnable {
 
 	private final AbstractBuffer<T> inputSorted;
@@ -11,7 +17,22 @@ public class InsertionSort<T extends BufferEntry<T>> implements Runnable {
 	private final AbstractBuffer<T> outputUnsorted;
 	private final InsertionSortManager<T> manager;
 
-	public InsertionSort(final AbstractBuffer<T> inputSorted, final AbstractBuffer<T> inputUnsorted, final AbstractBuffer<T> outputSorted, final InsertionSortManager<T> manager) {
+	/**
+	 * Initializes the object.
+	 * 
+	 * @param inputSorted
+	 *            A buffer that contains already sorted elements.
+	 * @param inputUnsorted
+	 *            A buffer that contains unsorted elements.
+	 * @param outputSorted
+	 *            An output buffer for sorted elements.
+	 * @param manager
+	 *            The manager which will take care of the sorting process.
+	 */
+	public InsertionSort(final AbstractBuffer<T> inputSorted,
+			final AbstractBuffer<T> inputUnsorted,
+			final AbstractBuffer<T> outputSorted,
+			final InsertionSortManager<T> manager) {
 		this.inputSorted = inputSorted;
 		this.inputUnsorted = inputUnsorted;
 		this.outputSorted = outputSorted;
@@ -25,7 +46,7 @@ public class InsertionSort<T extends BufferEntry<T>> implements Runnable {
 		if (element.isStopCommand()) {
 			this.end();
 			return;
-		} 
+		}
 		this.manager.startNewThread(this.outputSorted, this.outputUnsorted);
 		T current = this.inputUnsorted.get();
 		while (!current.isStopCommand()) {
@@ -33,14 +54,14 @@ public class InsertionSort<T extends BufferEntry<T>> implements Runnable {
 			current = this.inputUnsorted.get();
 		}
 		this.outputUnsorted.put(current);
-		
+
 		current = this.inputSorted.get();
 		while (!current.isStopCommand() && current.compareTo(element) <= 0) {
 			this.outputSorted.put(current);
 			current = this.inputSorted.get();
 		}
 		this.outputSorted.put(element);
-		
+
 		while (!current.isStopCommand()) {
 			this.outputSorted.put(current);
 			current = this.inputSorted.get();
@@ -49,19 +70,25 @@ public class InsertionSort<T extends BufferEntry<T>> implements Runnable {
 		this.manager.threadEnds();
 	}
 
+	/**
+	 * Moves all entries from <inputSorted> to <outputSorted>.
+	 */
 	private void end() {
 		T current = this.inputSorted.get();
 		while (!current.isStopCommand()) {
 			this.outputSorted.put(current);
 			current = this.inputSorted.get();
 		}
-		
-		this.outputSorted.put(current);		// stop command
-		this.outputUnsorted.put(current); 	// stop command
-		
+
+		this.outputSorted.put(current); // stop command
+		this.outputUnsorted.put(current); // stop command
+
 		this.manager.threadEnds();
 	}
 
+	/**
+	 * Starts the sorting process.
+	 */
 	public void start() {
 		new Thread(this).start();
 	}
