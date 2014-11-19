01fb9844d87601e98409847d2062b946f8500413
ole.vonbargen@googlemail.com <ole.vonbargen@googlemail.com@c11406e9-745a-c773-d383-491c0784de40>
2014-11-19 13:33:56 +0000

    - Added Philosopher - working - missing javadocs

---
+++ Philosopher/src/test/PTOTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import model.PTOMonitor;
+
+import org.junit.Test;
+
+public class PTOTest {
+
+	@Test
+	public void test() {
+		final PTOMonitor monitor = PTOMonitor.getMonitor();
+		
+		monitor.addPhilosopher(500);
+		monitor.startSimulation(1000 * 5);
+		
+		
+	}
+
+}
